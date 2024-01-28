5fe046e4be4a286fcd95f2e420f563188cbb6c07
Julyana Salvador <julyanaolacerda@gmail.com>
2024-01-27 23:31:28 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+const botao = document.querySelector(".btn-plataforma");
+
+const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+
+botao.addEventListener("click", () => {
+
+   elementoPlataformas.classList.toggle("ativo");
+   
+   /*     const botaoEstaAberto = elementoPlataformas.classList.contains("ativo");
+    if(botaoEstaAberto){
+        elementoPlataformas.classList.remove("ativo");
+    }else{
+        elementoPlataformas.classList.add("ativo");
+    } */
+} )
